12a04d3d86b5e416f44f3ab21b9a04bca7c54fbc
Elena <anel92r@gmail.com>
2020-03-01 21:23:30 +0100

    add build

---
+++ build/precache-manifest.bc134ef59ed145ff15bb6ffd5be0456a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbecfdeeffbaa8073f5dfd330dc2927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f11cb7381842e2aed4c",
+    "url": "/static/css/main.656f0744.chunk.css"
+  },
+  {
+    "revision": "1675533bd1bbd4af5f95",
+    "url": "/static/js/2.599f8ddc.chunk.js"
+  },
+  {
+    "revision": "5f11cb7381842e2aed4c",
+    "url": "/static/js/main.fc01d86f.chunk.js"
+  },
+  {
+    "revision": "ac6a728e4049d7390b68",
+    "url": "/static/js/runtime-main.395db9b5.js"
+  }
+]);
